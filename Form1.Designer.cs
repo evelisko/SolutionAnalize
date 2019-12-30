33d5c521401a1f2aeeb71e9ab4526dfd3fae7adc
Sergey.Keldibekov <sergey.keldibekov@russianpos.ru>
2019-12-30 15:07:16 +0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace SolutionAnalize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbSolutionFolder = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbWorkFolder = new System.Windows.Forms.TextBox();
+            this.tbCompillngFolder = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnFolderCopmpare = new System.Windows.Forms.Button();
+            this.btnProjTree = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbSolutionFolder
+            // 
+            this.tbSolutionFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSolutionFolder.Location = new System.Drawing.Point(120, 90);
+            this.tbSolutionFolder.Margin = new System.Windows.Forms.Padding(2);
+            this.tbSolutionFolder.Name = "tbSolutionFolder";
+            this.tbSolutionFolder.Size = new System.Drawing.Size(533, 20);
+            this.tbSolutionFolder.TabIndex = 29;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(657, 84);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 31);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbLog
+            // 
+            this.tbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbLog.Location = new System.Drawing.Point(9, 146);
+            this.tbLog.Margin = new System.Windows.Forms.Padding(2);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbLog.Size = new System.Drawing.Size(822, 628);
+            this.tbLog.TabIndex = 32;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Каталог с проектом";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Рабочее решение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 13);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "Скомпиллированный проект";
+            // 
+            // tbWorkFolder
+            // 
+            this.tbWorkFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbWorkFolder.Location = new System.Drawing.Point(162, 47);
+            this.tbWorkFolder.Margin = new System.Windows.Forms.Padding(2);
+            this.tbWorkFolder.Name = "tbWorkFolder";
+            this.tbWorkFolder.Size = new System.Drawing.Size(491, 20);
+            this.tbWorkFolder.TabIndex = 38;
+            // 
+            // tbCompillngFolder
+            // 
+            this.tbCompillngFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCompillngFolder.Location = new System.Drawing.Point(162, 11);
+            this.tbCompillngFolder.Margin = new System.Windows.Forms.Padding(2);
+            this.tbCompillngFolder.Name = "tbCompillngFolder";
+            this.tbCompillngFolder.Size = new System.Drawing.Size(491, 20);
+            this.tbCompillngFolder.TabIndex = 37;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(657, 41);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 31);
+            this.button3.TabIndex = 36;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(657, 5);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 31);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnFolderCopmpare
+            // 
+            this.btnFolderCopmpare.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFolderCopmpare.Location = new System.Drawing.Point(699, 5);
+            this.btnFolderCopmpare.Margin = new System.Windows.Forms.Padding(2);
+            this.btnFolderCopmpare.Name = "btnFolderCopmpare";
+            this.btnFolderCopmpare.Size = new System.Drawing.Size(129, 67);
+            this.btnFolderCopmpare.TabIndex = 34;
+            this.btnFolderCopmpare.Text = "Сравнить каталоги ";
+            this.btnFolderCopmpare.UseVisualStyleBackColor = true;
+            this.btnFolderCopmpare.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnProjTree
+            // 
+            this.btnProjTree.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnProjTree.Location = new System.Drawing.Point(699, 80);
+            this.btnProjTree.Name = "btnProjTree";
+            this.btnProjTree.Size = new System.Drawing.Size(131, 39);
+            this.btnProjTree.TabIndex = 42;
+            this.btnProjTree.Text = "Построить дерево проекта";
+            this.btnProjTree.UseVisualStyleBackColor = true;
+            this.btnProjTree.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 796);
+            this.Controls.Add(this.btnProjTree);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbWorkFolder);
+            this.Controls.Add(this.tbCompillngFolder);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnFolderCopmpare);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.tbSolutionFolder);
+            this.Controls.Add(this.button2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSolutionFolder;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbWorkFolder;
+        private System.Windows.Forms.TextBox tbCompillngFolder;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnFolderCopmpare;
+        private System.Windows.Forms.Button btnProjTree;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SolutionAnalize
+{
+    public partial class Form1 : Form
+    {
+        public struct FileAttrib
+        {
+          public string fileName;
+          public long fileSize;
+          public string description;
+        }
+
+        public struct Solution
+        {
+            public string solutionName;
+            public List<string> projects;
+        }
+
+        string[] projectsAll;  // список всех проектов в EAS-POS.
+        string EAS_path;
+        List<FileAttrib> FilesCompiling;
+        List<FileAttrib> FilesWork;
+        string filesPathCompilling;
+        string filesPathWork;
+        string filesPathProject;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            FilesCompiling = new List<FileAttrib>();
+            FilesWork = new List<FileAttrib>();
+            btnProjTree.Enabled = false; 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbDialogFileA = new FolderBrowserDialog();
+
+            if (fbDialogFileA.ShowDialog() == DialogResult.OK)
+            {
+              
+                tbSolutionFolder.Text = fbDialogFileA.SelectedPath;
+                filesPathProject = fbDialogFileA.SelectedPath;
+            }
+        }
+
+        //     Нужно название файла решения
+        //   - список входящих в решение файлов. 
+        //   
+
+        // Выполняет сравнение файлов решений. 
+        private void SolutionAnalize(string solutionFolder)
+        {
+            EAS_path = solutionFolder;
+        
+            projectsAll = Directory.GetFiles(EAS_path, "*.csproj", SearchOption.AllDirectories);
+
+            List<Solution> solutions = SolutionList(EAS_path); // создаем список решений
+
+            foreach (Solution solution in solutions)
+            {
+                tbLog.AppendText(solution.solutionName + "\r\n");
+                foreach (string prg in solution.projects.ToArray())
+                {
+                    tbLog.AppendText("          " + prg + "\t" + ProgComarator(prg) + "\r\n"); // выводи результат работы функции. 
+                }
+            }
+
+            tbLog.AppendText("\r\nСписок_проектов_не_пренадлежащих_ни_к_одному_решению \r\n\r\n");
+
+            //  Нужно указывать имя проекта без пути
+
+            int i;
+            foreach (string prgName in projectsAll)
+            {
+                i = 0;
+                foreach (Solution solution in solutions)
+                {
+                    foreach (string prg in solution.projects.ToArray())
+                    {
+                        string s2 = Path.GetFileName(prg);
+
+                        string s3 = Path.GetFileName(prgName);
+
+                        if (s2 == s3) i++;
+                    }
+                }
+                if (i == 0)
+                {
+                  tbLog.AppendText(prgName + "\t" + ProgComarator(prgName) + "\r\n");
+                }
+            }
+
+
+        }
+
+        // Создает список решений. 
+        //
+        private List<Solution> SolutionList(string path)
+        {
+            string[] solutionArray = Directory.GetFiles(EAS_path, "*.sln", SearchOption.AllDirectories);
+
+            List<Solution> solutions = new List<Solution>();
+            Solution solution = new Solution();
+
+            foreach (string solutionStr in solutionArray)
+            {
+                solution.solutionName = Path.GetFileName(solutionStr);
+                solution.projects = progectsArray(solutionStr);
+                solutions.Add(solution);
+            }
+
+            return solutions;
+        }
+
+        // выполняет разбор внутренней структуры файла sln. создает список проектов входящий в решение
+        private List<string> progectsArray(string path)
+        {
+            List<string> projectsInSolution = new List<string>(); // string[1];  // список всех проектов в eas-pos.  
+                                                                  //   Array.Resize()  
+                                                                  //   SetLength(projectsInSolution = 
+
+            // string[] allfilesa = directory.getfiles(filespatha, "*.cs", searchoption.alldirectories);
+
+            StreamReader f = new StreamReader(path);
+            while (!f.EndOfStream)
+            {
+                string s = f.ReadLine();
+                // читаем новую строку. 
+                // ищем в строке название файла и возвращаем результат. 
+                string s1 = stringAnalizer(s);
+                if (s1.Length > 0)
+                    projectsInSolution.Add(s1);
+            }
+            f.Close();
+            // производим построковый анализ файла. 
+            // находим имя проекта в строке 
+
+            return projectsInSolution;
+        }
+
+        private string stringAnalizer(string str)
+        {
+            string substr = "";
+
+            string[] strings = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in strings)
+            {
+                if (s.Contains(".csproj"))
+                {
+                    substr = s.Trim(new char[] { ',', '\"' });
+                    break;
+                }
+            }
+            return substr;
+        }
+
+
+
+        private string ProgComarator(string projName)
+        {
+            string resultStr = "";
+            // создаем список файлов имеющих расширение exe или dll. 
+            // при заполнении таблицы указываем из размер 
+            // Если файла не существует в скомпилированном и рабочем проекте, то ничего не пишем. "" // КР--- 
+            // Если файл существует в рабочем решении, а в скомпилированном нет, то Р--- 
+            // если размеры равны  ===  
+            // Если размер не равны пишем разницу СК - Р
+            // ,где СК - скомпилированный проект. 
+            // Р - работающий проект полученный из установщика.  
+
+            // нужно выполнить предварительную подготовку.  
+            projName = Path.GetFileNameWithoutExtension(projName);
+
+
+            foreach (FileAttrib fa in FilesWork.ToArray())
+            {
+                if (Path.GetFileNameWithoutExtension(fa.fileName) == projName)
+                {
+                    string strEx = Path.GetExtension(fa.fileName);
+                    resultStr = strEx + "  " + fa.description;
+                    break;
+                }
+            }
+            return resultStr;
+        }
+
+        private void FolderComparator()
+        {
+            //       string nameFileA;
+            //       string sizeFileA;
+            FileAttrib fA = new FileAttrib();
+            // запускаем поиск файлов расположенных в текущем каталоге. с учетом подкаталогов. 
+         
+            string[] allfilesA = Directory.GetFiles(filesPathCompilling, "*.*", SearchOption.AllDirectories);
+            FilesWork.Clear();
+            FilesCompiling.Clear(); 
+            // возникает ошибка при индексировании. 
+            // Нужно проводить индексирование файлов в каждома каталоге
+            // Добавлять в каталог файл сразу при получении его имени.
+            // При ошибке доступа к каталогу выводить соответствующее исключение. 
+            // выводить разрешение файла
+
+            for (int indexA = 0; indexA < allfilesA.Length; indexA++)
+            {
+                fA.fileName = Path.GetFileName(allfilesA[indexA]);
+                System.IO.FileInfo fi = new FileInfo(allfilesA[indexA]);
+                fA.fileSize = fi.Length;
+                // добавляем файлы в массив.
+                string strEx = Path.GetExtension(allfilesA[indexA]);
+                if (strEx != ".cs")
+                    FilesCompiling.Add(fA);
+            }
+            string[] allfilesB = Directory.GetFiles(filesPathWork, "*.*", SearchOption.AllDirectories);
+
+            for (int indexB = 0; indexB < allfilesB.Length; indexB++)// foreach (string filename in allfilesB)
+            {
+                fA.fileName = Path.GetFileName(allfilesB[indexB]);
+                System.IO.FileInfo fi = new FileInfo(allfilesB[indexB]);
+                fA.fileSize = fi.Length;// (allfilesB[indexB]).Length; //attributes. File.filу (allfilesB[indexB]);//. e.GetAttributes.Si System.IO.
+                // добавляем файлы в массив.
+                if (Path.GetExtension(allfilesB[indexB]) != ".cs")
+                    FilesWork.Add(fA);
+            }
+
+            long index = 0;
+
+            for (int indexB = 0; indexB < FilesWork.Count; indexB++)
+            {
+                for (int indexA = 0; indexA < FilesCompiling.Count; indexA++)
+                {
+                    if (FilesCompiling[indexA].fileName == FilesWork[indexB].fileName)
+                    {
+                        if (FilesCompiling[indexA].fileSize != FilesWork[indexB].fileSize)
+                        {
+                            index++;
+                            fA.fileName = FilesWork[indexB].fileName;
+                            fA.fileSize = FilesWork[indexB].fileSize;
+                            fA.description = (FilesWork[indexB].fileSize - FilesCompiling[indexA].fileSize).ToString();
+                            FilesWork[indexB] = fA; 
+                            tbLog.AppendText(index.ToString() + " " + FilesWork[indexB].fileName + "\t" + FilesWork[indexB].description+"\r\n");
+
+                            //   dGvFileList[3, dGvFileList.Rows.Count - 1].Value = ;
+                        }
+                        else
+                        {
+                            fA.description = "==="; // (FilesWork[indexB].fileSize - FilesCompiling[indexA].fileSize).ToString();
+                            fA.fileName = FilesWork[indexB].fileName;
+                            fA.fileSize = FilesWork[indexB].fileSize;
+                            FilesWork[indexB] = fA;
+                        }
+                        continue;
+                    }
+                }
+            }
+
+
+            int matches = 0;   
+
+          for (int indexB = 0; indexB < FilesWork.Count; indexB++)
+            {
+                matches = 0;
+                for (int indexA = 0; indexA < FilesCompiling.Count; indexA++)
+                {
+                    if (FilesCompiling[indexA].fileName == FilesWork[indexB].fileName)
+                    {
+                        matches++;
+                    }
+                }
+
+                if (matches == 0)
+                {
+                    index++;
+                    fA.description = "P---"; 
+                    fA.fileName = FilesWork[indexB].fileName;
+                    fA.fileSize = FilesWork[indexB].fileSize;
+                    FilesWork[indexB] = fA;
+                    tbLog.AppendText(index.ToString() + " " + FilesWork[indexB].fileName + "\t" + FilesWork[indexB].description+"\r\n");
+                }
+            }
+//            label1.Text = "Вывод результата";
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // проводим сравнение файлов в каталогах. 
+            // определяем разницу в размерах файлов в проектах и их наличие. 
+            //    
+            FolderComparator();
+            btnProjTree.Enabled = true; 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbDialogFileA = new FolderBrowserDialog();
+            if (fbDialogFileA.ShowDialog() == DialogResult.OK)
+            {
+                filesPathCompilling = fbDialogFileA.SelectedPath;
+                tbCompillngFolder.Text = filesPathCompilling;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbDialogFileB = new FolderBrowserDialog();
+            if (fbDialogFileB.ShowDialog() == DialogResult.OK)
+            {
+              filesPathWork = fbDialogFileB.SelectedPath;
+              tbWorkFolder.Text = filesPathWork;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           if (filesPathProject!="")
+            {
+              tbLog.Text = "";
+              SolutionAnalize(filesPathProject);
+            }
+        }
+        // Производит поиск проектов не входящих ни водно решение.   
+    }
+}
